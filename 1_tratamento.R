e095eae230e05f2a548b3ee879052d5d2555c208
luucasac <lucas.alvarenga92@gmail.com>
2019-02-24 13:32:59 -0300

    Adicionado o script para fazer o tratamento de dados, a aplicacao shiny que vou utilizar para apresentar os dados e o Rproj

---
+++ 1_tratamento.R
@@ -0,0 +1,12 @@
+#
+#  ---- AVALIAÇOES DIAGNÓSTICAS ---- #
+# 
+# Manipulacao dos dados e tratamento
+#
+# importando as bibliotecas necessarias
+
+library(readxl)
+
+# importando os dados xlsx
+
+r <- read_xlsx("../Avaliacoes_diagnosticas/9F826100.xlsx", sheet = 1)
